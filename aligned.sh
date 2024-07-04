c4422e674735b521dfc060e086984e80a8cd3fda
W99 <52006421+Wissrutt99@users.noreply.github.com>
2024-07-04 13:31:28 +0700

    aligned.sh

--- aligned.sh
+++ aligned.sh
@@ -7,15 +7,13 @@ NC='\033[0m'
 
 echo -e "\n${BLUE}Installing aligned_layer and setting up environment...${NC}\n"
 curl -L https://raw.githubusercontent.com/yetanotherco/aligned_layer/main/batcher/aligned/install_aligned.sh | bash
-source /home/gitpod/.bashrc  # Ensure .bashrc is sourced to update PATH
+source /root/.bashrc  # .bashrc is sourced to update PATH
 
 echo -e "\n${BLUE}Getting proof test files...${NC}\n"
 curl -L https://raw.githubusercontent.com/yetanotherco/aligned_layer/main/batcher/aligned/get_proof_test_files.sh | bash
 
 echo -e "\n${BLUE}Removing existing verification data (if any)...${NC}\n"
 rm -rf ~/.aligned/aligned_verification_data/
-
-export PATH="$PATH:/home/gitpod/.aligned/bin"
 
 echo -e "\n${BLUE}Submitting proof to aligned_layer...${NC}\n"
 aligned submit \
