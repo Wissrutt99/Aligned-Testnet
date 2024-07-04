2259c2a1f3d7bb1f6d96faf7afddd038331e9fca
W99 <52006421+Wissrutt99@users.noreply.github.com>
2024-07-04 13:41:21 +0700

    aligned.sh

--- aligned.sh
+++ aligned.sh
@@ -12,10 +12,8 @@ source /root/.bashrc  # .bashrc is sourced to update PATH
 echo -e "\n${BLUE}Getting proof test files...${NC}\n"
 curl -L https://raw.githubusercontent.com/yetanotherco/aligned_layer/main/batcher/aligned/get_proof_test_files.sh | bash
 
-echo -e "\n${BLUE}Removing existing verification data (if any)...${NC}\n"
-rm -rf ~/.aligned/aligned_verification_data/
-
 echo -e "\n${BLUE}Submitting proof to aligned_layer...${NC}\n"
+rm -rf ~/.aligned/aligned_verification_data/ &&
 aligned submit \
 --proving_system SP1 \
 --proof ~/.aligned/test_files/sp1_fibonacci.proof \
@@ -23,7 +21,7 @@ aligned submit \
 --aligned_verification_data_path ~/.aligned/aligned_verification_data \
 --conn wss://batcher.alignedlayer.com
 
-sleep 60
+sleep 10
 
 echo -e "\n${BLUE}Verifying proof onchain...${NC}\n"
 aligned verify-proof-onchain \
